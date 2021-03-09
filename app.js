23ca1d6a51b228a1c9f5691b32165507d1fdad45
Lucas Soares <lucasoaresleite@gmail.com>
2021-03-09 15:28:17 -0300

    impl logger

--- app.js
+++ app.js
@@ -1,22 +1,30 @@
 const fs = require('fs');
-//Bring all files from the campaigns folder
+
 const sleep = require('system-sleep');
+const logger = require('./util/logConfig')
 
-
-//This function will repeat itself to seek changes in the folder
+//This function will repeat itself to seek changes in the folder and bring all files that are on it
 function getFiles() {
     var files = fs.readdirSync('campaigns');
     var timer = 1;//Seconts that the program will wait for the new files to arrive via ftp
     //For each mapped file you can perform individual action using the callback
     for (timer; timer > 0; timer--) {
-        console.log(`Aguardando ${timer} segundos para continuar...`);
+        logger.log({
+            level: 'verbose',
+            message: `Aguardando ${timer} segundos para continuar...`
+        })
         sleep(1000);
     }
 
     var mappedFiles = files.map((file) => {
-        console.log(`Arquivo: ${file} pronto para processamento.`);
+        logger.log(
+            {
+                level: 'verbose',
+                messae: `Arquivo: ${file} pronto para processamento.`
+            }
+        )
     });
-    
-    setTimeout(getFiles, timer*1000);
+
+    setTimeout(getFiles, timer * 1000);
 }
 getFiles();
