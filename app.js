25efc579ff76603783a7e10afc52a589ec47dc62
Lucas Soares <lucasoaresleite@gmail.com>
2021-03-09 13:59:11 -0300

    update package

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const fs = require('fs');
 //Bring all files from the campaigns folder
 var files = fs.readdirSync('campaigns');
+const sab = new SharedArrayBuffer(1024);
+const int32 = new Int32Array(sab);
 
 //For each mapped file you can perform individual action using the callback
-var mappedFiles = files.map((file)=>{
-   console.log(file);
-});
+function getFiles() {
+    var mappedFiles = files.map((file) => {
+        console.log(file);
+    });
+
+}
